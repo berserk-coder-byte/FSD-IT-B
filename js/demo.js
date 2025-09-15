1908946f085db0b9560670d5affb08ee68fd72de
berserk-coder-byte <kaushikvihu26@gmail.com>
2025-09-15 12:11:15 +0530

    new ones

---
+++ js/demo.js
@@ -0,0 +1,29 @@
+// console.log(a);
+// var a=5;
+// console.log(a);
+// let b=1,c=12;
+// const d=13;
+// {
+//     let e=14;
+//     let f=15;
+//     var g=17
+//     console.log(a)
+//     console.log(b)
+//     console.log(c)
+//     console.log(d)
+//     console.log(e)
+//     f++
+//     console.log(f)
+//     // console.log(a)
+// }
+// // console.log(e);
+// console.log(g);
+  
+
+// Types of Functions in JS
+// 1.Normal function
+// 2.Arrow Function
+// 3.Anonymous Function
+// 4.Callback Function:-using this we can pass the function as a parameter to another function.
+// 5.IIFE Function
+// 
